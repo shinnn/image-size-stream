aaa41df8d1614d430615bd372fbb21bf4725e956
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-27 22:58:24 +0900

    use through2 instead of through
    close #1

--- index.js
+++ index.js
@@ -2,44 +2,60 @@
  * image-size-stream | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/image-size-stream
 */
-
 'use strict';
 
-var through = require('through');
+var through = require('through2');
+var tryit = require('tryit');
 var imageSize = require('image-size');
 
-var LIMIT = 128 * 1024;
+var DEFAULT_LIMIT = 128 * 1024;
 
-module.exports = function createImageSizeStream() {
-  var buffer = new Buffer([]);
+module.exports = function createImageSizeStream(option) {
+  option = option || {};
+  if (option.limit !== undefined) {
+    if (typeof option.limit !== 'number') {
+      throw new TypeError(
+        option.limit +
+        ' is not a number. `limit` option must be a number.'
+      );
+    }
+  } else {
+    option.limit = DEFAULT_LIMIT;
+  }
+
+  var buffer = new Buffer(0);
   var dimensions;
   var detectionError;
-  
-  return through(function write(chunk) {
-    buffer = Buffer.concat([buffer, chunk]);
+
+  return through(function detectImageSize(chunk, enc, cb) {
     if (!dimensions) {
-      try {
+      buffer = Buffer.concat([buffer, chunk]);
+
+      tryit(function() {
         dimensions = imageSize(buffer);
-      } catch (e) {
-        detectionError = e;
-        if (buffer.length > LIMIT) {
-          this.emit('error', detectionError);
-        }
-      }
+      }, function(err) {
+        detectionError = err;
+      });
+
       if (dimensions) {
         this.emit('size', dimensions);
+      } else if (buffer.length > option.limit) {
+        this.emit('error', new Error('Reached the limit before detecting image type.'));
       }
     }
-    this.queue(chunk);
-  }, function end() {
+
+    this.push(chunk);
+    cb();
+  }, function flush() {
+    if (dimensions) {
+      return;
+    }
+
     if (buffer.length === 0) {
       this.emit('error', new Error('No bytes received.'));
       return;
     }
-    if (!dimensions) {
-      this.emit('error', detectionError);
-      return;
-    }
-    this.emit('end');
+
+    this.emit('error', detectionError);
   });
 };
